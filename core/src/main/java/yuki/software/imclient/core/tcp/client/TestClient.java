fa073e99fa8c3b7e51f3f19938dcae9264776274
YuKiSuperMan <1040929773@qq.com>
2018-11-13 17:42:02 +0800

    client 创建项目

---
+++ core/src/main/java/yuki/software/imclient/core/tcp/client/TestClient.java
@@ -0,0 +1,18 @@
+package yuki.software.imclient.core.tcp.client;
+
+/**
+ * @programN: imclient
+ * @description: 测试客户端
+ * @author: YuKi
+ * @create: 2018-11-12 23:46
+ * @email: 1040929773@qq.com
+ */
+public class TestClient {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
